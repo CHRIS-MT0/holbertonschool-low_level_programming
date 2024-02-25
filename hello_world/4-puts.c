dc68a44a81c9ee3e3f7d317c4607555a3f0b70b3
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-02-25 16:09:04 +0000

    Corrigiendo detalles

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-/*Este comando imprimira un mensaje*/
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
--- hello_world/README.md
+++ hello_world/README.md
@@ -1,1 +1,1 @@
-:)
+In this project, each phase of compiling a C file will be carried out step by step.
