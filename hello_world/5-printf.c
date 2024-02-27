82d3f9d39d6ddc6cab3a9373a47477918b5dafa4
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-02-27 18:11:34 +0000

    Corrigiendo

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/*Comment*/
+/**
+*main - Entry point
+*
+*Return: Always returns 0
+*
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
