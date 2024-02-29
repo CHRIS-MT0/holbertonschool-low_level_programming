b48e6ae7edcc6e9a62f57d4aa3cbb095115f0694
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-02-29 15:28:11 +0000

    !

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  */
 void print_alphabet(void)
 {
-char n = 0;
+  char n;
 
-for (n = 'a'; n <= 'z'; n++)
-{
-_putchar(n);
-}
-
-_putchar('\n');
+  for (n = 'a'; n <= 'z'; n++)
+    {
+      _putchar(n);
+    }
+  _putchar('\n');
 }
