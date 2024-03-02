50cf1f4d3c8d3084241b89a6b13a5f0e73afd2b0
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-02 15:52:08 +0000

    lo q sea

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul - function to multiply 2 digits.
+ *Return: multiply the 2 digits.
+ * @a: first number.
+ * @b: second number.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
