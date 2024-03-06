8217442bd32e9185dd30ba2f4f06ddd90dec7baf
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-06 20:40:32 +0000

    s

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add - suma dos numero enteros
+ *@a: first number
+ *@b: second number
+ *Return: la suma de dos numeros enteros
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
