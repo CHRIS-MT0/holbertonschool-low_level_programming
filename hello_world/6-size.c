41cf771b17ce9722b65478657a3dfc9bd80c0c85
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-07 17:40:39 +0000

    push

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,16 +8,10 @@
 */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-printf("Size of a char: %zu bytes\n", sizeof(a));
-printf("Size of an int: %zu bytes\n", sizeof(b));
-printf("Size of a long int: %zu bytes\n", sizeof(c));
-printf("Size of a long long int: %zu bytes\n", sizeof(d));
-printf("Size of a float: %zu bytes\n", sizeof(e));
+printf("Size of a char: %zu bytes\n", sizeof(char));
+printf("Size of an int: %zu bytes\n", sizeof(int));
+printf("Size of a long int: %zu bytes\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of a float: %zu bytes\n", sizeof(float));
 return (0);
 }
