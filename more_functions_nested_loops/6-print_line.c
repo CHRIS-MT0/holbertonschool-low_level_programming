78fabd2aa55222ab26ba8d64c81c4ef40c293414
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-13 16:28:36 +0000

    s

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_line - print _
+ *@n: character to print
+ */
+void print_line(int n)
+{
+char a;
+if (n == 0)
+{
+putchar('\n');
+}
+for (a = 0; a < n;  a++)
+putchar('_');
+{
+putchar('\n');
+}
+}
