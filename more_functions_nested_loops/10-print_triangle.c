a26449edbea29fbc32fcedc99501994958a23264
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-15 19:34:04 +0000

    s

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,20 +2,22 @@
 #include <unistd.h>
 
 /**
+ *print_triangle - print to triangle
+ *@size: print * to the triangle
  */
 void print_triangle(int size)
 {
-  int a, j, k;
-  for (a = 1; a <= size; a++)
-    {
-      for (k = 1; k <= size -a; k++)
-	{
-	  write(STDOUT_FILENO, " ", 1);
-	}
-      for (j = 1; j <= a; j++)
-	{
-	  write(STDOUT_FILENO, "*", 1);
-	}
-      write(STDOUT_FILENO, "\n", 1);
-    }
+int a, j, k;
+for (a = 1; a <= size; a++)
+{
+for (k = 1; k <= size - a; k++)
+{
+write(STDOUT_FILENO, " ", 1);
+}
+for (j = 1; j <= a; j++)
+{
+write(STDOUT_FILENO, "*", 1);
+}
+write(STDOUT_FILENO, "\n", 1);
+}
 }
