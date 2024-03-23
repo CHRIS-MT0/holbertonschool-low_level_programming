8cbbf6ba875a32a689e834f0dcf6edc0438bc0b9
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-23 19:02:49 +0000

    s

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
+ * _strlen - Calculate the length of a strings.
+ * @s: number to character
  */
 int _strlen(char *s)
 {
-  int a = 0;
-  while (*s != '\0')
-    {
-      a++;
-      s++;
-    }
-  return a;
+int a = 0;
+while (*s != '\0')
+{
+a++;
+s++;
+}
 }
