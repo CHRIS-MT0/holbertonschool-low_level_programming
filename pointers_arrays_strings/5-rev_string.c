8870e5afd78003bbf1c1310ec83c2dba14fd3893
CHRIS-MT0 <christophermoralestorres013@gmail.com>
2024-03-24 20:57:53 +0000

    s

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,13 @@
  */
 void rev_string(char *s)
 {
+  int i, j;
+  char temp;
   int length = 0;
   while(s[length] != '\0')
     {
       length++;
     }
-    int i, j;
-    char temp;
     for (i = 0, j = length - 1; i < j; i++, j--)
       {
         temp = s[i];
